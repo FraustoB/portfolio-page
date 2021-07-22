41685d5b0e20bb6d7a604bd2a88ee3c462c285b8
FraustoB <brandon.frausto18@gmail.com>
2021-07-21 22:01:55 -0500

    adjusted Portfolio page, adjusted univeral css looks on each page, made adjusttments for ipad screen viewing

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,10 +1,29 @@
 import './css/Portfolio.css';
+import PortfolioCard from './PortfolioCard';
+import PortfolioInfo from './Portfolio.info';
 
 
 export default function Portfolio() {
+
+    const { WonTonSoup, ApiTranslator }=PortfolioInfo;
+
     return (
         <div className='Portfolio'>
-            This is where my Portfolio Projects will go
+            <div className='Portfolio-container'>
+                <PortfolioCard
+                    className='Portfolio-cards'
+                    title={WonTonSoup.title}
+                    description={WonTonSoup.description}
+                    liveSite={WonTonSoup.liveSite}
+                    sourceCode={WonTonSoup.sourceCode} />
+
+                <PortfolioCard
+                    className='Portfolio-cards'
+                    title={ApiTranslator.title}
+                    description={ApiTranslator.description}
+                    liveSite={ApiTranslator.liveSite}
+                    sourceCode={ApiTranslator.sourceCode} />
+            </div>
         </div>
     )
 }
