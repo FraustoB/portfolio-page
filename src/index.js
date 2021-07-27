f97a059d638342b3ccd863075267c2a2abeac1b0
FraustoB <brandon.frausto18@gmail.com>
2021-07-26 19:25:10 -0500

    Attempted to update branch to fix server error problems

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import FraustoApp from './Frausto.App';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <FraustoApp />
   </BrowserRouter>,
   document.getElementById('root')
 );
